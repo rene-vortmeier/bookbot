66c56def0e1dc274b71393e7d60f81e994c4b0d0
rene-vortmeier <rene.vortmeier@googlemail.com>
2025-08-31 15:15:12 +0200

    003: add .gitignore update main.py get book text

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def main():
+    location="./books/frankenstein.txt"
+    print(get_book_text(location))
+    return None
+
+main()
