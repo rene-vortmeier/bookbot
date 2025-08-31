aebea9206b2a0de18484e90322a5ec140be1e52a
rene-vortmeier <rene.vortmeier@googlemail.com>
2025-08-31 16:02:36 +0200

    0006: cleaning the code, count only alphabetic letter

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from stats import get_book_text,count_words,count_letters
 
 def main():
     path="./books/frankenstein.txt"
-    words=0
-    words=count_words(get_book_text(path))
-    letters=count_letters(get_book_text(path))
+    text=get_book_text(path)
+    words=count_words(text)
+    letters=count_letters(text)
     print(f"{words} words found in the document")
     for letter in letters:
         print(f"'{letter}': {letters[letter]}")
